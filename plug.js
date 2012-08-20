5f90c0972eb20b4c9683539dbe079b3d6082871f
Punkred <penguin305k@gmail.com>
2012-08-20 10:28:51 -0300

    Update plug.js

--- plug.js
+++ plug.js
@@ -565,8 +565,8 @@ $('#plugbot-js').remove();
  */
 $('body').prepend('<style type="text/css" id="plugbot-css">' 
 	+ '#plugbot-ui { position: absolute; margin-left: 349px; }'
-	+ '#plugbot-ui p { background-color: #0b0b0b; height: 32px; padding: 8px 0 0 12px; cursor: pointer; font-variant: small-caps; width: 85px; font-size: 13px; margin: 0; }'
-	+ '#plugbot-ui h2 { background-color: #0b0b0b; height: 71px; width: 164px; margin: 0; color: #fff; font-size: 13px; font-variant: small-caps; padding: 8px 0 0 12px; }'
+	+ '#plugbot-ui p { background-color: #0b0b0b; height: 32px; width: 85px; padding: 8px 0 0 12px; cursor: pointer; font-variant: small-caps; font-size: 13px; margin: 0; }'
+	+ '#plugbot-ui h2 { background-color: #0b0b0b; height: 71px; width: 164px; padding: 8px 0 0 12px; color: #fff; font-variant: small-caps; font-size: 13px; margin: 0; }'
 //    + '#plugbot-userlist { border: 6px solid rgba(10, 10, 10, 0.8); border-left: 0 !important; background-color: #000000; padding: 8px 0px 20px 0px; width: 12%; }'
 //    + '#plugbot-userlist p { margin: 0; padding-top: 2px; text-indent: 24px; font-size: 10px; }'
 //    + '#plugbot-userlist p:first-child { padding-top: 0px !important; }' padding-top: 8px; padding-left: 12px;

1f17434505b6416de9adf99e1df15e340ecfc3f2
Punkred <penguin305k@gmail.com>
2012-08-20 10:53:19 -0300

    Update plug.js

--- plug.js
+++ plug.js
@@ -565,7 +565,7 @@ $('#plugbot-js').remove();
  */
 $('body').prepend('<style type="text/css" id="plugbot-css">' 
 	+ '#plugbot-ui { position: absolute; margin-left: 349px; }'
-	+ '#plugbot-ui p { background-color: #0b0b0b; height: 20px; width: 85px; padding: 8px 0 0 12px; cursor: pointer; font-variant: small-caps; font-size: 13px; margin: 0; }'
+	+ '#plugbot-ui p { background-color: #0b0b0b; height: 25px; width: 95px; padding: 8px 0 0 12px; cursor: pointer; font-variant: small-caps; font-size: 14px; margin: 0; }'
 	+ '#plugbot-ui h2 { background-color: #0b0b0b; height: 71px; width: 164px; padding: 8px 0 0 12px; color: #fff; font-variant: small-caps; font-size: 13px; margin: 0; }'
 //    + '#plugbot-userlist { border: 6px solid rgba(10, 10, 10, 0.8); border-left: 0 !important; background-color: #000000; padding: 8px 0px 20px 0px; width: 12%; }'
 //    + '#plugbot-userlist p { margin: 0; padding-top: 2px; text-indent: 24px; font-size: 10px; }'
